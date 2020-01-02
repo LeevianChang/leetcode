4b033f0b3b00e2d6da8a29b617695ea51bc4cf96
leevianchang <leevianchang@gmail.com>
2020-01-02 13:49:15 +0800

    47. 全排列 II

--- 47. 全排列 II/main.py
+++ 47. 全排列 II/main.py
@@ -1,7 +1,8 @@
 class Solution:
-    def permute(self, nums):
+    def permuteUnique(self, nums):
         resList = []
         # for i in range(len(nums)):
+        nums.sort()
         self.permuteRecursion(nums,len(nums),resList,[])
         return resList
     def permuteRecursion(self,nums,length,resList,currList):    
@@ -10,15 +11,16 @@ class Solution:
             return
         
         for i in range(len(nums)):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
             currList.append(nums[i])
             # 把当前遍历的值去除，再递归
             self.permuteRecursion(nums[:i]+nums[i+1:],length,resList,currList)
             currList.pop()
-         
         
 
 
             
 s = Solution()
-f = s.permute([1,2,3])
+f = s.permuteUnique([1,1,2])
 print(f)
